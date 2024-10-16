ef87986d5e225a0617d70a54fe57ed80457e4cc5
Mahammad Ashad <143719204+Mr-Ashad@users.noreply.github.com>
2024-10-16 13:03:23 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -83,7 +83,7 @@ function App() {
       setMessages(prevMessages => [
         ...prevMessages,
         { text: greetingOrGoodbyeResponse, isBot: true },
-        { text: InputData, isBot: false},
+        { text: inputData, isBot: false},
       ]);
       setInputData('');
       return; // Exit early if we have a greeting or goodbye
